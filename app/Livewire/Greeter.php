9732b2b7cb6513966d14aa546c770e48af174ee5
Dennis Oostrom <336914@student.mboutrecht.nl>
2025-01-13 16:00:55 +0100

    les 2 af

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -7,6 +7,11 @@ use Livewire\Component;
 class Greeter extends Component
 {
     public $name = 'Joe';
+
+    public function changeName($newName){
+        $this->name = $newName;
+    }
+
     public function render()
     {
         return view('livewire.greeter');
